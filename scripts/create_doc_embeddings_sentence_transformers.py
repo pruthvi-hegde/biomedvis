d07211f09da20085f7b768577262663fd5db2228
Pruthvi <pruthvihegde93@gmail.com>
2022-08-09 12:39:46 +0200

    Add all changes

--- scripts/create_doc_embeddings_sentence_transformers.py
+++ scripts/create_doc_embeddings_sentence_transformers.py
@@ -43,8 +43,20 @@ def high_low_dim_emb_for_sentence_tranformer_models_with_mean(model_name):
     model_name = model_name.split('/')[-1].replace('-', '_').lower()
 
     # To encode the papers, we must combine the title and the abstracts to a single string
-    article_texts = [paper['article_title'] + '[SEP]' + paper['abstract'] for paper in papers]
+    article_texts = [paper['article_title'] + '. ' + paper['abstract'] for paper in papers]
     article_titles = [paper['article_title'] for paper in papers]
+
+    # If you would like to average sentence embeddings
+    # doc_emb = []
+    # for article in article_texts:
+    #     article_sentences = article.split('. ')
+    #     emb = [model.encode(sentence).tolist() for sentence in article_sentences]
+    #     doc_emb.append(list(np.mean(emb, axis=0)))
+    #
+    # umap_embeddings = UMAP(n_neighbors=5, n_components=2, metric='cosine', random_state=42)
+    # low_dim_embeddings = umap_embeddings.fit_transform(doc_emb)
+    # low_dim_embeddings = low_dim_embeddings.tolist()
+    # result_high = dict(zip(article_titles, doc_emb))
 
     # Compute embeddings for all papers
     corpus_embeddings = model.encode(article_texts, convert_to_tensor=True)
@@ -65,5 +77,5 @@ def high_low_dim_emb_for_sentence_tranformer_models_with_mean(model_name):
 
 
 if __name__ == "__main__":
-    # high_low_dim_emb_for_sentence_tranformer_models('all-mpnet-base-v2')
-    high_low_dim_emb_for_sentence_tranformer_models('allenai-specter')
+    # high_low_dim_emb_for_sentence_tranformer_models('allenai-specter')
+    high_low_dim_emb_for_sentence_tranformer_models('all-mpnet-base-v2')
