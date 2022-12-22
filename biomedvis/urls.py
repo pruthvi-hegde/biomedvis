3753f75491c446f1094f28ff70ee39139d08fd72
pruthvi <pruthvihegde93@gmail.com>
2022-12-22 14:19:30 +0100

    changed settings

--- biomedvis/urls.py
+++ biomedvis/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from . import settings
+from biomedvis import settings
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
